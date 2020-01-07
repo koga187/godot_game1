daeac3150fcc2a66219804ef9977bc2d42aae235
Bruno Koga <brunokoga187@gmail.com>
2020-01-07 12:10:48 -0300

    Fase 2 - iniciando com tileSet

--- Player.gd
+++ Player.gd
@@ -1,16 +1,30 @@
 extends KinematicBody2D
 
+const UP = Vector2(0, -1) ## -1 é acima da imagem
+const JUMP_HEIGHT = -550
+const SPEED = 200
+const GRAVITY_SPEED = 20
 var motion = Vector2()
 
 func _physics_process(delta):
-	
-	motion.y += 20
+	gravity()
+	walk()
+	jump()
 
+	motion = move_and_slide(motion, UP)
+
+func gravity():
+	motion.y += GRAVITY_SPEED
+
+func walk():
 	if Input.is_action_pressed("ui_right"):
-		motion.x = 100
+		motion.x = SPEED
 	elif Input.is_action_pressed("ui_left"):
-		motion.x = -100
+		motion.x = -SPEED
 	else:
 		motion.x = 0
 
-	move_and_slide(motion)
+func jump():
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_up"):
+			motion.y = JUMP_HEIGHT
